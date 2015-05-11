20c99eb38ab983ba9b82f7a96decdfa8b06b67c1
JHarb52207 <jharb52207@gmail.com>
2015-05-11 17:12:07 -0500

    Added refiner texture added NEI for refiner

--- src/main/java/fluxedCrystals/nei/FluxedCrystalsNEIConfig.java
+++ src/main/java/fluxedCrystals/nei/FluxedCrystalsNEIConfig.java
@@ -21,8 +21,8 @@ public class FluxedCrystalsNEIConfig implements IConfigureNEI
 	public void loadConfig() {
 		API.registerRecipeHandler(new GemCutterHandler());
 		API.registerUsageHandler(new GemCutterHandler());
-//		API.registerRecipeHandler(new GemRefinerHandler());
-//		API.registerUsageHandler(new GemRefinerHandler());
+		API.registerRecipeHandler(new GemRefinerHandler());
+		API.registerUsageHandler(new GemRefinerHandler());
 		API.registerRecipeHandler(new InfuserRecipeHandler());
 		API.registerUsageHandler(new InfuserRecipeHandler());
 	}
