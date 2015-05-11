b239278c310cf8da3223d747d37ef1560d7c2646
jaredlll08 <jaredlll08@gmail.com>
2015-05-11 10:32:54 +0200

    Sharp works now, someone messed up...

--- src/main/java/fluxedCrystals/blocks/crystal/BlockCrystal.java
+++ src/main/java/fluxedCrystals/blocks/crystal/BlockCrystal.java
@@ -13,6 +13,7 @@ import fluxedCrystals.util.DamageSourceCrystal;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockCactus;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -36,19 +37,20 @@ public class BlockCrystal extends CrystalBase implements ITileEntityProvider, IW
 		setBlockTextureName(Reference.LOWERCASE_MOD_ID + ":crop_stage_7");
 	}
 
-//	@Override
-//	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_) {
-//		return null;
-//	}
+	// @Override
+	// public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_,
+	// int p_149668_2_, int p_149668_3_, int p_149668_4_) {
+	// return null;
+	// }
 
 	@Override
 	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
 		TileEntityCrystal crop = (TileEntityCrystal) world.getTileEntity(x, y, z);
 
 		if (SeedRegistry.getInstance().getSeedByID(crop.getIdx()).isSharp) {
-			if (!world.isRemote && world.getWorldTime() % 5 == 0)
+			if (!world.isRemote)
 				if (entity instanceof EntityPlayer)
-					entity.attackEntityFrom(new DamageSourceCrystal(), world.getBlockMetadata(x, y, z));
+					entity.attackEntityFrom(new DamageSourceCrystal(), 1.0f);
 		}
 	}
 
--- src/main/java/fluxedCrystals/handler/PlayerEventHandler.java
+++ src/main/java/fluxedCrystals/handler/PlayerEventHandler.java
@@ -1,20 +1,89 @@
 package fluxedCrystals.handler;
 
+import java.util.Random;
+
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.event.ServerChatEvent;
+
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import fluxedCrystals.network.PacketHandler;
 import fluxedCrystals.network.message.MessageSyncSeeds;
 import fluxedCrystals.registry.SeedRegistry;
-import net.minecraft.entity.player.EntityPlayerMP;
 
-public class PlayerEventHandler
-{
+public class PlayerEventHandler {
+	public boolean resetRender;
+	public float trans = 0;
+	public boolean descending;
+
+	
+	@SubscribeEvent
+	public void chat(net.minecraftforge.event.entity.player.PlayerEvent.NameFormat e){
+		if(e.displayname.equalsIgnoreCase("Jaredlll08")){
+			e.displayname = EnumChatFormatting.BLUE + "Jared" + EnumChatFormatting.RESET;
+		}
+	}
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void render(RenderLivingEvent.Pre event) {
+		String s = EnumChatFormatting.getTextWithoutFormattingCodes(event.entity.getCommandSenderName());
+		if (s.equals("Jaredlll08") || s.equals("esriel123") || s.equalsIgnoreCase("parcel31u")) {
+			if (new Random().nextInt(2) == 0) {
+				if (!descending) {
+					trans++;
+				} else {
+					trans--;
+				}
+				if (trans > 100) {
+					descending = true;
+					trans = 100f;
+				}
+				if (trans < 0) {
+					descending = false;
+					trans = 0;
+				}
+			}
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, trans / 100);
+			GL11.glEnable(3042);
+			GL11.glBlendFunc(770, 771);
+			resetRender = true;
+		}
+
+	}
 
 	@SubscribeEvent
-	public void onPlayerLoggedIn(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event)
-	{
+	public void entityColorRender(RenderLivingEvent.Post event) {
+		if (this.resetRender) {
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glDisable(3042);
+			resetRender = false;
+		}
+	}
 
-		if (event.player != null)
-		{
+	public static void render3DItem(EntityItem item) {
+
+		GL11.glPushMatrix();
+		GL11.glDepthMask(true);
+		GL11.glRotatef(90, 90, 0, 1);
+
+		item.hoverStart = 0.0F;
+
+		RenderManager.instance.func_147939_a(item, 0.0, -0.1, -0.3, 0, 0, false);
+		GL11.glPopMatrix();
+	}
+
+	@SubscribeEvent
+	public void onPlayerLoggedIn(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event) {
+
+		if (event.player != null) {
 
 			PacketHandler.INSTANCE.sendTo(new MessageSyncSeeds(SeedRegistry.getInstance().getSeedMap()), (EntityPlayerMP) event.player);
 
